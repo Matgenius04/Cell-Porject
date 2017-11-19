469fa362fe67404bea31728c9c030358c7d56050
F4Tornado <h.rovnyak@gmail.com>
2017-11-18 21:23:35 -0500

    yeeeeeee

--- epic.js
+++ epic.js
@@ -1,8 +1,16 @@
 function setup() {
   createCanvas(600, 400).position(700, 100);
-  background(0);
 }
 
+let text;
+
 function draw() {
-  background(255, 122, 0);
+  background(255);
+  text(text)
+}
+
+function showInfo(type) {
+  if (type === 'nucleus') {
+    text = 'Nucleus: This is the organelle that stores the genetic material for a cell.'
+  }
 }
