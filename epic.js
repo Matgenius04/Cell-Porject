2f4a4381815fe320613aa5bc3748314329ffe8a7
F4Tornado <h.rovnyak@gmail.com>
2017-11-18 21:17:43 -0500

    ORANGE

--- epic.js
+++ epic.js
@@ -1,8 +1,8 @@
 function setup() {
-  createCanvas(600, 400).position(0, 0);
+  createCanvas(600, 400).position(600, 100);
   background(0);
 }
 
 function draw() {
-  background(0);
+  background(255, 122, 0);
 }
